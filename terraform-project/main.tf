5c656d6617299e20fa707f5261e579992de9653e
ash2code <144456748+ash2code@users.noreply.github.com>
2024-03-15 23:48:07 +0530

    Update main.tf

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -1,23 +1,30 @@
-provider "aws" { 
-    region = "us-east-1"
-}
-
-resource "aws-instance" "my-ec2" {
-    ami = "ami-07d9b9ddc6cd8dd30"
-    instance_type = "t2.micro"
-
-    tags = {
-        Names = "my-ec2"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"  # Specify the version constraint based on compatibility
     }
+  }
 }
 
-resource "aws_s3_bucket" "my-s3" {
-    bucket = "my-cicd-s3-test-bucket"
-
-    tags = {
-        Name = "my-cicd-s3-test-bucket"
-        Environment = "Dev"
-    }  
+provider "aws" {
+  region = "us-east-1"
 }
 
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-07d9b9ddc6cd8dd30"
+  instance_type = "t2.micro"
 
+  tags = {
+    Name = "my-ec2"
+  }
+}
+
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "my-cicd-s3-test-bucket"
+
+  tags = {
+    Name        = "my-cicd-s3-test-bucket"
+    Environment = "Dev"
+  }
+}
